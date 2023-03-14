690feaa3cffd57bfdfb1a8174062472e4587a6f9
YII <lkbnk@outlook.com>
2023-03-14 19:32:02 +0300

    change

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+def task1():
+    a = int(input('Введите число: '))
+    if a % 3 == 0:
+        print('Число делится на 3')
+    else:
+        print('Число не делится на 3')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+task1()
