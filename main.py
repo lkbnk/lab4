9d8919227b0c22fe02aadc4e6dbde4a9256d1627
YII <lkbnk@outlook.com>
2023-03-14 19:35:05 +0300

    change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def task1():
-    a = int(input('Введите число: '))
+def task1(a):
     if a % 3 == 0:
         print('Число делится на 3')
     else:
         print('Число не делится на 3')
-
 task1()
